0c030d96248eac912abc1eb768769fcd31e139d2
PRIME AZTECS <emmanuelonyeike33@gmail.com>
2025-02-13 14:52:48 +0100

    Update card.js

--- card.js
+++ card.js
@@ -1,13 +1,1 @@
-  
-        function showResult(resultId) {
-            // Clear all result cells
-            const allResults = document.querySelectorAll('.result-cell');
-            allResults.forEach(cell => {
-                cell.style.display = 'none';
-            });
 
-            // Show the clicked result
-            const resultCell = document.getElementById(resultId);
-            resultCell.style.display = 'block';
-        }
-    
