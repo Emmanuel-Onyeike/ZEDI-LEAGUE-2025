209389ef09671adad91a5ccc0c20f22173f438b1
PRIME AZTECS <emmanuelonyeike33@gmail.com>
2025-02-14 01:07:32 +0100

    Update card.js

--- card.js
+++ card.js
@@ -1,1 +1,9 @@
 
+  function revealContent() {
+            document.getElementById("myModal").style.display = "flex";
+        }
+
+        // Function to close the modal
+        function closeModal() {
+            document.getElementById("myModal").style.display = "none";
+        }
